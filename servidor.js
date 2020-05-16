d8138f3c7b6b16d897769507cfb933b602ee1329
jose <jose_armandoam@outlook.es>
2020-05-16 19:53:28 +0200

    Servidor con mongodb
    Añadido mongoDB junto con socket.io

--- servidor.js
+++ servidor.js
@@ -2,8 +2,9 @@ const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient;
 
-server.listen(8080);
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/login.html')
@@ -28,5 +29,26 @@ app.get('/imgs/:imagen', function(req, res){
 app.get('/css/:css', function(req, res){
   res.sendFile(__dirname + '/css/'+req.params.css)
 })
+
+const uri = "mongodb://localhost:27017/";
+MongoClient.connect(uri, function(err, db) {
+	server.listen(8080);
+	var dbo = db.db("socialugr");
+	dbo.createCollection("test", function(err, collection){
+    	io.sockets.on('connection',
+      function(client) {
+        client.emit('my-address', {host:client.request.connection.remoteAddress,
+          port:client.request.connection.remotePort});
+        client.on('poner', function (data) {
+          collection.insert(data, {safe:true}, function(err, result) {});
+        });
+        client.on('obtener', function (data) {
+          collection.find(data).toArray(function(err, results){
+            client.emit('obtener', results);
+          });
+        });
+      });
+    });
+});
 
 console.log("Servicio Socket.io iniciado");
